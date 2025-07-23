257e91912b24a4fd5232444097926ec8a37798dd
zgildo01 <zzaragozagildo@gmail.com>
2025-07-22 20:47:00 -0400

    add first div to landing page, add image of myself

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div>
-      Errrm... check please??
+      <Image src="/me.jpg" 
+        alt="A picture of Zeus, a software and DevOps engineer" 
+        width={200} 
+        height={200}
+        className="rounded-full mx-auto mb-4"
+      />
+      <h1 className="text-center">Welcome to my portfolio!</h1>
+      <p className="text-center">I&#39;m Zeus, a software and DevOps engineer.</p>
     </div>
   );
 }
